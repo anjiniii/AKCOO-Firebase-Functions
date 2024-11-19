fd3d48a7f397e0d31766c4bfa0c9b92b3237d212
Anjin Lee <anjin720jin@naver.com>
2024-11-19 16:18:11 +0900

    Feat: 물가정보 업데이트 handler 구현

---
+++ src/handlers/updateCountryPrice.ts
@@ -0,0 +1,88 @@
+import { onRequest } from "firebase-functions/https";
+import { sendErrorResponse, sendSuccessResponse } from "../utils/responseUtil";
+import { getFirestore } from "firebase-admin/firestore";
+
+// 숙박, 식당, 카페 각각의 세부 항목 정의
+export interface AccommodationDTO {
+  guesthouse: number;
+  "3Star": number;
+  "4Star": number;
+}
+
+export interface RestaurantDTO {
+  local: number;
+  casualDining?: number;
+}
+
+export interface CafeDTO {
+  local: number;
+  starbucks: number;
+}
+
+// prices 전체 구조 정의
+export interface PricesDTO {
+  accommodation: AccommodationDTO;
+  restaurant: RestaurantDTO;
+  cafe: CafeDTO;
+}
+
+export interface CountryPricesUpdateBodyDTO {
+  countryId: string;
+  prices: PricesDTO;
+}
+
+// Firestore에 데이터를 추가하는 HTTP 트리거 함수
+export const updateCountryPriceHandler = onRequest(async (req, res) => {
+  try {
+    // 요청 메서드 검증
+    if (req.method !== "POST") {
+      sendErrorResponse(res, 405, "Only POST requests are allowed.");
+      return;
+    }
+
+    // 요청 본문 검증
+    const body: CountryPricesUpdateBodyDTO = req.body;
+    const { countryId, prices } = body;
+
+    // "text" 파라미터가 없는 경우 오류 반환
+    if (!countryId || !prices) {
+      sendErrorResponse(
+        res,
+        400,
+        "Both body contents - 'countryId' and 'prices' - are required."
+      );
+      return;
+    }
+
+    // prices 위치
+    const pricesRef = getFirestore()
+      .collection("countries")
+      .doc(countryId as string)
+      .collection("prices");
+
+    // Firebase Admin SDK를 사용하여 Firestore에 데이터 추가
+    // 각 카테고리와 세부 항목 업데이트
+    for (const [category, priceData] of Object.entries(prices)) {
+      for (const [field, value] of Object.entries(priceData as object)) {
+        await pricesRef.doc(category).update({
+          [field]: value,
+        });
+      }
+    }
+
+    // 성공 메시지 반환
+    sendSuccessResponse(
+      res,
+      200,
+      `${new Date().toISOString()}`,
+      "성공적으로 요청을 완료했습니다.",
+      body
+    );
+  } catch (error) {
+    sendErrorResponse(
+      res,
+      500,
+      "Firestore에 환율 정보를 업데이트하지 못했습니다."
+    );
+  }
+});
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 // Firebase Admin SDK to access Firestore
 import { initializeApp } from "firebase-admin/app";
 import { updateExchangeRateHandler } from "./handlers/updateExchangeRate";
+import { updateCountryPriceHandler } from "./handlers/updateCountryPrice";
 
 // Firebase Admin SDK 초기화
 initializeApp();
 
-// Firestore에 데이터를 추가하는 HTTP 트리거 함수
+// Firestore에 환율 정보 업데이트
 export const updateExchangeRate = updateExchangeRateHandler;
+
+// Firestore에 물가 정보 업데이트
+export const updateCountryPrice = updateCountryPriceHandler;
